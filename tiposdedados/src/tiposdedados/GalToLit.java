40790c1d1d91796b4fc435a00559db803318bd43
Samira <tisamirasilva@gmail.com>
2021-02-21 02:57:16 -0300

    correção de erro ortográfico

--- tiposdedados/src/tiposdedados/GalToLit.java
+++ tiposdedados/src/tiposdedados/GalToLit.java
@@ -1,18 +1,18 @@
 package tiposdedados;
 
-//programa pra converter galões em litros
+//programa para converter galões em litros
 
 public class GalToLit {
 
 	public static void main(String[] args) {
 
 		double gallons; // variável do tipo double contendo o núnero de galões
-		double liters; // variável do tipo double contém a conversão para litros
+		double liters; // variável do tipo double que contém a conversão para litros
 
 		gallons = 10; // representa 10 galões
 		liters = gallons * 3.7854; // representa a conversão para litros
 
-		System.out.println(gallons + " gallons is " + liters + " liters."); // intrução para a saída de dados no console
+		System.out.println(gallons + " gallons is " + liters + " liters."); // instrução para a saída de dados no console
 
 	}
 
