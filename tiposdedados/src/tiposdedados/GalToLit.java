a86cd011061388ee2cea4a379b6e5d01f2f21a1b
Samira <tisamirasilva@gmail.com>
2021-02-21 02:55:07 -0300

    criado programa que converte galões em litros

---
+++ tiposdedados/src/tiposdedados/GalToLit.java
@@ -0,0 +1,19 @@
+package tiposdedados;
+
+//programa pra converter galões em litros
+
+public class GalToLit {
+
+	public static void main(String[] args) {
+
+		double gallons; // variável do tipo double contendo o núnero de galões
+		double liters; // variável do tipo double contém a conversão para litros
+
+		gallons = 10; // representa 10 galões
+		liters = gallons * 3.7854; // representa a conversão para litros
+
+		System.out.println(gallons + " gallons is " + liters + " liters."); // intrução para a saída de dados no console
+
+	}
+
+}
