b518e815fd2770a616abdd35b268f68c8a1a8036
Samira <tisamirasilva@gmail.com>
2021-02-21 02:08:17 -0300

    programa demostrativo para a declaração de variável do tipo int

---
+++ tiposdedados/src/tiposdedados/Example1.java
@@ -0,0 +1,21 @@
+package tiposdedados;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		int var1; // declarando a primeira variável do tipo int (valor inteiro)
+		int var2; // declarando outra variável do tipo int (valor inteiro)
+		
+		var1 = 1024; // valor atribuído à variável var1
+		
+		System.out.println("var1 contains " + var1);  //instrução pra gerar valor de  var1
+		
+		var2 = var1 /2; // o valor de var2 é o valor de var1 dividido por 2 (1024/2= 512)
+		
+		System.out.print("var2 contains var1 / 2:"); //instrução pra gerar valor de var2
+		System.out.println(var2);
+
+	}
+
+}
