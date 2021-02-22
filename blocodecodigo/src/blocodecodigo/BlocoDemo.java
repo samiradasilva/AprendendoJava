190e5ed00f912cde7e375b6c8d92c5a173a9b776
Samira <tisamirasilva@gmail.com>
2021-02-21 21:53:32 -0300

    demo de bloco de código alvo da instrução if

---
+++ blocodecodigo/src/blocodecodigo/BlocoDemo.java
@@ -0,0 +1,20 @@
+package blocodecodigo;
+
+public class BlocoDemo {
+
+	public static void main(String[] args) {
+
+		double s, d, x;
+
+		s = 5;
+		d = 10;
+
+		if (s != 0) {
+			System.out.println("s does not equal zero");
+			x = d / s;
+			System.out.println("d / s " + x);
+		}
+
+	}
+
+}
