0cdea0acea494c662b5fcf4f2325fd58872c8838
Samira <tisamirasilva@gmail.com>
2021-02-22 15:54:54 -0300

    programa demo caractere Unicode (16 bits)

---
+++ tiposdedados/src/tiposdedados/Caractere.java
@@ -0,0 +1,21 @@
+package tiposdedados;
+
+public class Caractere {
+
+	public static void main(String[] args) {
+		
+		char ch;
+		
+		ch = 'x';
+		System.out.println("ch contains " + ch); 
+		
+		ch++;
+		System.out.println("ch is now " + ch); 
+		
+		ch = 90;
+		System.out.println("ch is now " + ch);
+		
+
+	}
+
+}
