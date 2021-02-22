a912e862ff6d75c502991b4686e4eadb625e161a
Samira <tisamirasilva@gmail.com>
2021-02-21 21:37:23 -0300

    programa demo da instrução laço for

---
+++ instrucaocontrole/src/instrucaocontrole/Example2.java
@@ -0,0 +1,16 @@
+package instrucaocontrole;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		int count; 
+		
+		for(count =0; count < 5; count  = count+1) 
+			System.out.println("This is count: " + count);
+		
+		System.out.println("Done!");
+
+	}
+
+}
