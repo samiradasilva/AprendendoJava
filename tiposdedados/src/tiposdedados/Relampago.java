d73bc2d1182b0f72c673c9824e59f7374f2e4081
Samira <tisamirasilva@gmail.com>
2021-02-22 16:25:47 -0300

    calculo dist relâmpago - valor tipo double

---
+++ tiposdedados/src/tiposdedados/Relampago.java
@@ -0,0 +1,15 @@
+package tiposdedados;
+
+public class Relampago { //calcula distância em pés do som do relâmpago
+
+	public static void main(String[] args) {
+		
+		double distancia;
+		
+		distancia = 7.2 * 1100; 
+		
+		System.out.println("O som do relâmpago é " + distancia + " pés.");
+
+	}
+
+}
